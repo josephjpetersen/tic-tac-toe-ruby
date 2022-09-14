f6323781dab37bafe416d6721ace214745db3753
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-14 12:34:26 -0400

    Write board drawing methods in TicTicToe class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,56 @@
-class Board
+module Clear
+  def clear
+    print "\033[2J\033[H"
+  end
+end
+
+class TicTacToe
+  include Clear
+
+  attr_accessor :positions, :winning_combos
+  
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
+    @positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @winning_combos = [
+      [1, 2, 3], 
+      [4, 5, 6], 
+      [7, 8, 9], 
+      [1, 4, 7], 
+      [2, 5, 8], 
+      [3, 6, 9], 
+      [1, 5, 9], 
+      [3, 5, 7]
+    ]
+  end
+
   def draw_header
     puts "|=-=-=-=-=-=-=-=-=-=-=-=-=|"
-    puts "|=-=-=- TIC-TAC-TOE -=-=-=|"
+    puts "|=-=-= TIC-TAC-TOE! -=-=-=|"
     puts "|=-=-=-=-=-=-=-=-=-=-=-=-=|"
     puts ""
   end
 
   def draw_board
-    
+    puts " #{@positions[0]} | #{@positions[1]} | #{@positions[2]}   |"
+    puts "---+---+---  | #{@p1.player_symbol}: #{@p1.score}  (#{@p1.name})"
+    puts " #{@positions[3]} | #{@positions[4]} | #{@positions[5]}   |"
+    puts "---+---+---  | #{@p2.player_symbol}: #{@p2.score}  (#{@p2.name})"
+    puts " #{@positions[6]} | #{@positions[7]} | #{@positions[8]}   |"
+    puts ""
   end
 
+  def update_board
+    clear
+    draw_header
+    draw_board
+  end
 end
 
 class Player
+  include Clear
+
   attr_accessor :name, :player_symbol, :score
   
   def initialize (player_symbol)
@@ -34,11 +72,9 @@ class Player
     puts "#{player_symbol} player, what is your name?"
     @name = gets.chomp
   end
-
-  def clear
-    print "\033[2J\033[H"
-  end
 end
 
 p1 = Player.new("X")
 p2 = Player.new("O")
+game = TicTacToe.new(p1, p2)
+game.update_board
