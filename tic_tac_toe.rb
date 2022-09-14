95679bbe300c40d6c0b3c86ce468654a36685b34
Joseph Petersen <joseph.j.petersen@gmail.com>
2022-09-14 15:05:13 -0400

    Write basic play_game method in TicTacToe class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,7 +11,9 @@ class TicTacToe
   
   def initialize(p1, p2)
     @p1 = p1
+    @p1_selections = []
     @p2 = p2
+    @p2_selections = []
     @positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @winning_combos = [
       [1, 2, 3], 
@@ -23,6 +25,10 @@ class TicTacToe
       [1, 5, 9], 
       [3, 5, 7]
     ]
+    @has_winner = false
+    @p1_wins = false
+    @p2_wins = false
+    update_board
   end
 
   def draw_header
@@ -45,6 +51,97 @@ class TicTacToe
     clear
     draw_header
     draw_board
+  end
+
+  def play_game
+    p1_active = false
+    p2_active = false
+
+    until @has_winner
+      until @p2_active
+        puts "#{@p1.name}, enter a number to place an #{@p1.player_symbol}:"
+        selection = gets.chomp.to_i
+      
+        if (1..9).include?(selection) == false
+          update_board
+          next
+        elsif @positions[selection - 1] == "X" || @positions[selection - 1] == "O"
+          update_board
+          next
+        else 
+          @p1_selections.push(@positions[selection - 1])
+          @positions[selection - 1] = @p1.player_symbol
+          update_board
+          check_winner
+          @p2_active = true
+        end
+      end
+
+      until @p1_active
+        puts "#{@p2.name}, enter a number to place an #{@p2.player_symbol}:"
+        selection = gets.chomp.to_i
+      
+        if (1..9).include?(selection) == false
+          update_board
+          next
+        elsif @positions[selection - 1] == "X" || @positions[slection - 1] == "O"
+          update_board
+          next
+        else 
+          @p2_selections.push(@positions[selection - 1])
+          @positions[selection - 1] = @p2.player_symbol
+          update_board
+          check_winner
+          @p1_active = true
+        end
+      end
+    end
+
+    if @has_winner
+      if @p1_wins
+        @p1.score += 1
+        update_board
+        puts "Congrats #{@p1.name}! You win!"
+      elsif @p2_wins
+        @p2.score += 1
+        update_board
+        puts "Congrats #{@p2.name}! You win!"
+      else
+        puts "It's a draw!"
+        update_board
+      end
+    end
+
+    puts "Would you like to go again? [Y/n]"
+    
+    selection = gets.chomp.downcase
+  
+    if selection == "y"
+      @p1_selections = []
+      @p2_selections = []
+      @positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      @has_winner = false
+      @p1_wins = false
+      @p2_wins = false
+      update_board
+      play_game
+    else
+      update_board
+      puts "Come play again sometime, #{@p1.name} and #{@p2.name}!"
+    end
+  end
+
+  def check_winner
+    @winning_combos.each do |set|
+      if (set & @p1_selections).sort == set.sort
+        @has_winner = true
+        @p1_wins = true
+      end
+      if (set & @p2_selections).sort == set.sort
+        @has_winner = true
+        @p2_wins = true
+      end
+    end
   end
 end
 
@@ -77,4 +174,4 @@ end
 p1 = Player.new("X")
 p2 = Player.new("O")
 game = TicTacToe.new(p1, p2)
-game.update_board
+game.play_game
